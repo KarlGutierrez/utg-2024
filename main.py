9bbce42c0cce6ef15f1ab7744827f7e21d9a655b
KarlGutierrez <karl.gutierrez@amadeus.com>
2024-11-14 11:31:01 +0100

    Add next_to function and hard coded movements to beat the bronze league

--- main.py
+++ main.py
@@ -92,6 +92,20 @@ class Game:
 
 game: Game = Game()
 
+def next_to(x1, y1, x2, y2):
+    if x2 == x1 and y2 == y1 - 1:
+        return 'N'  # North
+    elif x2 == x1 and y2 == y1 + 1:
+        return 'S'  # South
+    elif x2 == x1 + 1 and y2 == y1:
+        return 'E'  # East
+    elif x2 == x1 - 1 and y2 == y1:
+        return 'W'  # West
+    else:
+        return None  # Not adjacent or not a cardinal direction
+
+counter = 0
+
 # game loop
 while True:
     game.reset()
@@ -142,11 +156,49 @@ while True:
 
         # Write an action using print
         # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-        my_last_organ = game.my_organs[-1]
-        my_last_organ_id = my_last_organ.id
+        parents_id_list = []
+        for organ in game.my_organs:
+            parents_id_list.append(organ.parent_id)
+        for organ in game.my_organs:
+            if organ.id not in parents_id_list:
+                my_last_organ = organ
+                my_last_organ_id = organ.id
+
+        my_last_organ_pos = my_last_organ.pos
+        my_last_organ_x, my_last_organ_y = my_last_organ_pos.x, my_last_organ_pos.y
 
         opp_last_organ = game.opp_organs[-1]
-        opp_pos = opp_last_organ.pos
-        opp_x, opp_y = opp_pos.x, opp_pos.y
+        opp_last_organ_pos = opp_last_organ.pos
+        opp_last_organ_x, opp_last_organ_y = opp_last_organ_pos.x, opp_last_organ_pos.y
 
-        print(f"GROW {my_last_organ_id} {opp_x} {opp_y} BASIC")
+        first_protein = game.free_proteins[0]
+        first_protein_pos = first_protein.pos
+        first_protein_x, first_protein_y = first_protein_pos.x, first_protein_pos.y
+
+        if not next_to(my_last_organ_x, my_last_organ_y, first_protein_x-1, first_protein_y) and game.my_proteins['C'] != 0:
+            print(f"GROW {my_last_organ_id} {first_protein_x-2} {first_protein_y} BASIC")
+        elif game.my_proteins['C'] != 0:
+            print(f"GROW {my_last_organ_id} {first_protein_x-1} {first_protein_y} HARVESTER E")
+        elif not game.grid.get_cell(Pos(first_protein_x-1, first_protein_y+1)).isWall and counter == 0:
+            counter += 1
+            print(f"GROW {my_last_organ_id} {my_last_organ_x} {first_protein_y+1} BASIC")
+        elif game.grid.get_cell(Pos(first_protein_x-1, first_protein_y+1)).isWall and counter == 0:
+            counter += 1
+            print(f"GROW {my_last_organ_id} {my_last_organ_x} {first_protein_y-1} BASIC")
+        elif counter == 1 or counter == 2:
+            counter += 1
+            print(f"GROW {my_last_organ_id} {first_protein_x+1} {my_last_organ_y} BASIC")
+        else:
+            for opp_organ in game.opp_organs:
+                opp_organ_pos = opp_organ.pos
+                opp_organ_x, opp_organ_y = opp_organ_pos.x, opp_organ_pos.y
+                if next_to(my_last_organ_x, my_last_organ_y, opp_organ_x, opp_last_organ_y):
+                    counter += 1
+            if counter == 3:
+                for cell in game.grid.cells:
+                    if not cell.isWall and not cell.protein and not cell.organ in game.opp_organs and not cell.organ in game.my_organs:
+                        empty_cell_x, empty_cell_y = cell.pos.x, cell.pos.y
+                        print(f"GROW {my_last_organ_id} {empty_cell_x} {empty_cell_y} BASIC")
+                        break
+            else:
+                print(f"GROW {my_last_organ_id} {opp_last_organ_x} {opp_last_organ_y} BASIC")
